26842a9ade285cac647c936ca68f4779f56bfa6f
aranajhonny <jhonnyjosearana@gmail.com>
2016-09-02 17:57:48 -0400

    add pacientesController

--- controllers/pacientesController.php
+++ controllers/pacientesController.php
@@ -1,8 +1,25 @@
 <?php
-/**/
+/*
+* controlador de pacientes
+*/
 class pacientesController {
     public function index() {
         require_once("views/registrar_paciente.php");
+    }
+
+    public function create() {
+            $nro_historia = $_POST['nro_historia'];
+            $ced_paciente = $_POST['ced_paciente'];
+            $primer_nombre = $_POST['primer_nombre'];
+            $segundo_nombre = $_POST['segundo_nombre'];
+            $primer_apellido = $_POST['primer_apellido'];
+            $segundo_apellido = $_POST['segundo_apellido'];
+            $fecha_nac = $_POST['fecha_nac'];
+            $tipo_sangre = $_POST['tipo_sangre'];
+            $tel_movil = $_POST['tel_movil'];
+            $tel_casa = $_POST['tel_casa'];
+            $direccion = $_POST['direccion'];
+            echo $nro_historia, $ced_paciente, $primer_nombre, $segundo_nombre, $primer_apellido, $segundo_apellido,  $fecha_nac, $tipo_sangre, $tel_casa, $tel_movil, $direccion;
     }
 }
 
--- views/registrar_paciente.php
+++ views/registrar_paciente.php
@@ -5,35 +5,35 @@
         <div class="row">
             <br>
             <br>
-            <form>
+            <form action="?controller=pacientes&action=create" method="post">
                 <div class="form-group col-md-4 col-md-offset-2">
                     <label for="nro_historia">Numero de historia</label>
-                    <input type="text" class="form-control" name="nro_historia" id="nro_historia" disabled="true">
+                    <input type="text" class="form-control" name="nro_historia" id="nro_historia" disabled="true" required="required">
                 </div>
                 <div class="form-group col-md-4">
                     <label for="ced_paciente">Cedula</label>
-                    <input type="text" class="form-control" name="ced_paciente" id="ced_paciente" placeholder="cedula">
+                    <input type="text" class="form-control" name="ced_paciente" id="ced_paciente" placeholder="cedula" required="required">
                 </div>
                 <div class="form-group col-md-4 col-md-offset-2">
                     <label for="primer_nombre">Primer Nombre</label>
-                    <input type="text" class="form-control" name="primer_nombre" id="primer_nombre" placeholder="primer nombre">
+                    <input type="text" class="form-control" name="primer_nombre" id="primer_nombre" placeholder="primer nombre" required="required">
                 </div>
                 <div class="form-group col-md-4">
                     <label for="segundo_nombre">Segundo Nombre</label>
-                    <input type="text" class="form-control" name="segundo_nombre" id="segundo_nombre" placeholder="segundo nombre">
+                    <input type="text" class="form-control" name="segundo_nombre" id="segundo_nombre" placeholder="segundo nombre" required="required">
                 </div>
                 <div class="form-group col-md-4 col-md-offset-2">
                     <label for="primer_apellido">Primer apellido</label>
-                    <input type="text" class="form-control" name="primer_apellido" id="primer_apellido" placeholder="primer apellido">
+                    <input type="text" class="form-control" name="primer_apellido" id="primer_apellido" placeholder="primer apellido" required="required">
                 </div>
                 <div class="form-group col-md-4">
                     <label for="segundo_apellido">Segundo apellido</label>
-                    <input type="text" class="form-control" name="segundo_apellido" id="segundo_apellido" placeholder="segundo apellido">
+                    <input type="text" class="form-control" name="segundo_apellido" id="segundo_apellido" placeholder="segundo apellido" required="required">
                 </div>
                 <div class="form-group col-md-4  col-md-offset-2" id="sandbox-container">
                     <label for="fecha_nac">Fecha de nacimiento</label>
                     <div class="input-group date">
-                        <input type="text" class="form-control" name="fecha_nac" id="fecha_nac" placeholder="DD/MM/AAAA">
+                        <input type="text" class="form-control" name="fecha_nac" id="fecha_nac" placeholder="DD/MM/AAAA" required="required">
                         <span class="input-group-addon">
                         <i class="glyphicon glyphicon-th"></i>
                       </span>
@@ -41,19 +41,19 @@
                 </div>
                 <div class="form-group col-md-4">
                     <label for="tipo_de_sangre">Tipo de sangre</label>
-                    <input type="text" class="form-control" id="tipo_sangre" name="tipo_sangre" placeholder="Tipo de sangre">
+                    <input type="text" class="form-control" id="tipo_sangre" name="tipo_sangre" placeholder="Tipo de sangre" required="required">
                 </div>
                 <div class="form-group col-md-4 col-md-offset-2 ">
                     <label for="tel_movil">Telefono movil</label>
-                    <input type="text" class="form-control" id="tel_movil" name="tel_movil" placeholder="04XX/0000000">
+                    <input type="text" class="form-control" id="tel_movil" name="tel_movil" placeholder="04XX/0000000" required="required">
                 </div>
                 <div class="form-group col-md-4 ">
-                    <label for="tel_casa">Password</label>
-                    <input type="password" class="form-control" id="tel_casa" name="tel_casa" placeholder="04XX/0000000">
+                    <label for="tel_casa">Telefono de casa</label>
+                    <input type="text" class="form-control" id="tel_casa" name="tel_casa" placeholder="04XX/0000000">
                 </div>
                 <div class="form-group col-md-8 col-md-offset-2 ">
                     <label for="Direccion">Direccion</label>
-                    <textarea name="" id="direccion" name="direccion" class="form-control" rows="3" required="required"></textarea>
+                    <textarea id="direccion" name="direccion" class="form-control" rows="3" required="required"></textarea>
                 </div>
                 <center>
                     <button type="submit" class="btn btn-default col-md-2 col-md-offset-5">Submit</button>
